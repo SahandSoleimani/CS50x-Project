ce032edc49e2da92ab08f0cfdc70cd746df46a3f
sahandirsolei <contact@sahandsoleimani.ir>
2024-07-11 12:47:45 +0330

    initial commit

--- app.py
+++ app.py
@@ -14,7 +14,7 @@ def get_db_connection():
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
-    render_template("index.html")
+    return render_template("index.html")
 
 if __name__ == '__main__':
     app.run(debug=True)
